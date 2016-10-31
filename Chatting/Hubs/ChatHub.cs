27d3dd7089346202ae71382681785b6bca16d30d
정광일 <laitdei@imc.co.kr>
2016-11-01 01:25:54 +0900

    initialize commit

---
+++ Chatting/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Chatting.Hubs
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void ClientToServer(string msg)
+        {
+            Clients.All.serverToClient(msg);
+        }
+    }
+}
